6e1533c94e6b18bc10b0c4821474eed54ae5e4de
ZhouSusan <szhou089@gmail.com>
2022-12-18 15:14:23 -0800

    Added GenericList and printed out the total sum from the list in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Object> GenericList = new List<Object>();
+            GenericList.Add(7);
+            GenericList.Add(28);
+            GenericList.Add(-1);
+            GenericList.Add(false);
+            GenericList.Add(true);
+            GenericList.Add('z');
+            GenericList.Add("table");
+
+            int Sum = 0;
+            for (int i = 0; i < GenericList.Count; i++)
+            {
+                System.Console.WriteLine(GenericList[i]);
+                if (GenericList[i] is int)
+                {
+                    Sum += (int) GenericList[i];
+                }
+            }
+            Console.WriteLine("Sum is {0}" ,Sum);
+        }
+    }
+}
